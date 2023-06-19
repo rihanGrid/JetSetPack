4acf4e2a89b284ee5a2bcccbcb3f404b11631db3
rihanGrid <rsah@griddynamics.com>
2023-06-19 11:56:38 +0530

    cors policy

--- jetsetpack/settings.py
+++ jetsetpack/settings.py
@@ -54,10 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:8000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#   'http://localhost:8000',
+# )
 
 ROOT_URLCONF = 'jetsetpack.urls'
 
